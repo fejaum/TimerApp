f22dc13dccad7af266568a4cb9d92df91c41e551
Luiz Fernando Maehana <luizmaehana@gmail.com>
2018-04-01 21:22:18 -0300

    Implementações e Alterações
    Correção do tamanho da fonte de acordo com o tamanho do dispositivo;
    Adição do *beep*;
    Melhoria no código das funções JS;

--- js/script.js
+++ js/script.js
@@ -1,3 +1,51 @@
-function init() {
+let m = 1,
+    s = 0,
+    t,
+    p = document.querySelector(".play"),
+    x = document.querySelector(".timer"),
+    y = document.querySelector("#tempo"),
+    b = document.querySelector("#audio");
 
+function start() {
+    let v = y.value;
+    reset( t );
+    if (v >= 1 && v <= 60) {
+        beep();
+        changeTime(v, 0);
+        m = v - 1;
+        s = 60;
+        t = setInterval( function() {
+            timer();
+        }, 1000 );
+    }
+}
+
+function beep() {
+    b.play();
+}
+
+function changeTime(m, s) {
+    let ms = (m < 10) ? "0" + m : m,
+        ss = (s < 10) ? "0" + s : s;
+    x.innerHTML = ms + ":" + ss;
+}
+
+function reset( t ) {
+    clearInterval( t );
+    x.innerHTML = "00:00";
+}
+
+function timer() {
+    if (s <= 3)
+        beep();
+    if (s <= 0 && m <= 0)
+        reset( t );
+    else {
+        s--;
+        if ( s === 0 && m !== 0 ) {
+            s = 60;
+            m--;
+        }
+        changeTime(m, s);
+    }	
 }
