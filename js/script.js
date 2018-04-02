6110c225c6dbbe944ba12d08bb58ec12d4198e51
Luiz Fernando Maehana <luizmaehana@gmail.com>
2018-04-01 22:30:38 -0300

    Funcionalidade dos Botões
    Comandos corrigidos, funcionalidade dos botões alteradas;

--- js/script.js
+++ js/script.js
@@ -8,11 +8,18 @@ let m = 1,
     f = document.querySelector(".beep-final"),
     h = document.querySelector("header"),
     g = document.querySelector("form"),
-    r = document.querySelector(".fullscreen-exit");
+    r = document.querySelector(".fullscreen-exit"),
+    a = document.querySelector(".play"),
+    d = document.querySelector(".stop"),
+    j = document.querySelector(".pause"),
+    k = false;
 
 function start() {
     let v = y.value;
     resetTimer( t );
+    a.disabled = true;
+    d.disabled = false;
+    j.disabled = false;
     if (v >= 1 && v <= 60) {
         beep();
         changeTime(v, 0);
@@ -32,36 +39,54 @@ function beep(w) {
 }
 
 function changeTime(m, s) {
-    let ms = (m < 10) ? (s === 60) ? "01" : "0" + m : m,
-        ss = (s < 10) ? "0" + s : (s === 60) ? "00" : s;
+    let ms,
+        ss;
+    ms = (s === 60) ? (m + 1) : m;
+    ms = (ms < 10) ? "0" + ms : ms;
+    ss = (s < 10) ? "0" + s : (s === 60) ? "00" : s;
     x.innerHTML = ms + ":" + ss;
 }
 
 function resetTimer( t ) {
+    a.disabled = false;
+    d.disabled = true;
+    j.disabled = true;
     clearInterval( t );
     x.innerHTML = "00:00";
 }
 
 function timer() {
-    if (s <= 0 && m <= 0) {
-        resetTimer( t );
-    }
-    else {
-        s--;
-        if ( s <= 3 && s > 0 && m == 0 )
-            beep();
-        if ( s <= 0 && m == 0 )
-            beep( true );
-        if ( s === 0 && m !== 0 ) {
-            s = 60;
-            m--;
+    if ( !k ) {
+        if (s <= 0 && m <= 0) {
+            resetTimer( t );
         }
-        changeTime(m, s);
-    }	
+        else {
+            s--;
+            if ( s <= 3 && s > 0 && m == 0 )
+                beep();
+            if ( s <= 0 && m == 0 )
+                beep( true );
+            if ( s === 0 && m !== 0 ) {
+                s = 60;
+                m--;
+            }
+            changeTime(m, s);
+        }
+    }
 }
 
 function pause() {
-
+    if ( k ) {
+        k = false;
+        a.disabled = true;
+        d.disabled = false;
+        j.disabled = false;
+    } else {
+        a.disabled = true;
+        d.disabled = true;
+        j.disabled = false;
+        k = true;
+    }
 }
 
 function full( show ) {
