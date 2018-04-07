bfdc5f466f5d98518ae6be8a3dd0cce534d77e71
Luiz Fernando Maehana <luizmaehana@gmail.com>
2018-04-07 01:42:15 -0300

    Implementando o Menu lateral e os Tipos de Cronometro

--- js/script.js
+++ js/script.js
@@ -1,30 +1,40 @@
-let Cronometro = function ( tempoMin, tempoSeg, tempoAtual, progressivo ) {
+let Cronometro = function ( tempoMin, tempoSeg, tempoAtual, progressivo, tipo ) {
     this.tempoMin       = tempoMin;
     this.tempoSeg       = tempoSeg;
     this.tempoAtual     = tempoAtual;
     this.progressivo    = progressivo;
+    this.tipo           = tipo
 }
 
-let min             = 0,
-    seg             = 3,
-    inputTempo      = document.querySelector("#tempo"),
-    inputRodadas    = document.querySelector("#rodadas"),
-    botaoPlay       = document.querySelector(".play"),
-    botaoStop       = document.querySelector(".stop"),
-    botaoPause      = document.querySelector(".pause"),
-    botaoTipo       = document.querySelector("input[name='tipo']"),
-    spanTimer       = document.querySelector(".timer"),
-    somBeep         = document.querySelector(".beep"),
-    somBeepFinal    = document.querySelector(".beep-final"),
-    exit            = document.querySelector(".fullscreen-exit"),
-    header          = document.querySelector("header"),
-    form            = document.querySelector("form"),
-    main            = document.querySelector("main"),
-    pause           = false,
+let min                     = 0,
+    seg                     = 0,
+    segs                    = 0,
+    inputTempoCro           = document.querySelector("#tempo_cronometro"),
+    inputRodadasCro         = document.querySelector("#rodadas_cronometro"),
+    inputAquecimentoSerie   = document.querySelector("#aquecimento_tempo"),
+    inputExercicioSerie     = document.querySelector("#exercicio_tempo"),
+    inputDescansoSerie      = document.querySelector("#descanso_tempo"),
+    inputCooldownSerie      = document.querySelector("#cooldown_tempo"),
+    inputCooldown           = document.querySelector("input[name='cooldown']"),
+    inputAquecimento        = document.querySelector("input[name='aquecimento']"),
+    botaoPlay               = document.querySelector(".play"),
+    botaoStop               = document.querySelector(".stop"),
+    botaoPause              = document.querySelector(".pause"),
+    botaoTipo               = document.querySelector("input[name='tipo']"),
+    spanTimer               = document.querySelector(".timer"),
+    somBeep                 = document.querySelector(".beep"),
+    somBeepFinal            = document.querySelector(".beep-final"),
+    exit                    = document.querySelector(".fullscreen-exit"),
+    spanTipo                = document.querySelector("span.tipo"),
+    header                  = document.querySelector("header"),
+    form                    = document.querySelector("form"),
+    main                    = document.querySelector("main"),
+    pause                   = false,
+    cronometro              = 0,
+    cronometros             = [],
+    tipoCronometro          = "cronometro",
     tempo,
-    tipoProgressivo,
-    cronometro = 0,
-    cronometros = [];
+    tipoProgressivo;
 
 function start() {
     resetTimer( tempo );
@@ -32,28 +42,67 @@ function start() {
     disable( botaoTipo );
     enable( botaoPause );
     enable( botaoStop );
-    if ( cronometros[cronometro].tempoAtual >= 1 && cronometros[cronometro].tempoAtual <= 60 ) {
-        if (cronometro === 0) {   
-            min = cronometros[0].tempoMin;
-            seg = cronometros[0].tempoSeg;  
-            tempo = setInterval( function() {
-                timer();
-            }, 1000 );
+    if (cronometro === 0) {   
+        min = cronometros[0].tempoMin;
+        seg = cronometros[0].tempoSeg;
+        segs = cronometros[0].tempoAtual;
+        mudarTipo();
+        tempo = setInterval( function() {
+            timer();
+        }, 1000 );
+    } else {
+        if ( cronometros[cronometro].progressivo )  {
+            changeTime( 0, 0 );
+            min = 0;
+            seg = 0;
+            segs = 0;
         } else {
-            if ( cronometros[cronometro].progressivo )  {
-                changeTime( 0, 0 );
-                min = 0;
-                seg = 0;
-            } else {
-                changeTime( cronometros[cronometro].tempoAtual, 0 );
-                min = cronometros[cronometro].tempoAtual - 1;
-                seg = 60;
-            }
-            tempo = setInterval( function() {
-                timer();
-            }, 1000 );
+            changeTime( cronometros[cronometro].tempoMin, cronometros[cronometro].tempoSeg );
+            min = cronometros[cronometro].tempoMin;
+            seg = cronometros[cronometro].tempoSeg;
+            segs = cronometros[cronometro].tempoAtual;
         }
-        
+        mudarTipo(cronometros[cronometro].tipo);
+        tempo = setInterval( function() {
+            timer();
+        }, 1000 );
+    }
+}
+
+function mudarTipo( tipo ) {
+    switch (tipo) {
+        case "aquecimento":
+            spanTipo.innerHTML = "Aquecimento";
+            main.className = "";
+            main.classList.add("yellow");
+            main.classList.add("lighten-5");
+        break;
+
+        case "exercicio":
+            spanTipo.innerHTML = "Exercício";
+            main.className = "";
+            main.classList.add("green");
+            main.classList.add("lighten-5");
+        break;
+
+        case "descanso":
+            spanTipo.innerHTML = "Descanso";
+            main.className = "";
+            main.classList.add("red");
+            main.classList.add("lighten-5");
+        break;
+
+        case "cooldown":
+            spanTipo.innerHTML = "Cooldown";
+            main.className = "";
+            main.classList.add("blue");
+            main.classList.add("lighten-5");
+        break;
+
+        default :
+            spanTipo.innerHTML = "3, 2, 1!";
+            main.className = "";
+        break;
     }
 }
 
@@ -88,20 +137,18 @@ function resetTimer( resetTempo ) {
 function timer() {
     if ( !pause ) {
         if ( cronometros[cronometro].progressivo ) {
-            if (seg == 0 && min == cronometros[cronometro].tempoAtual) {
+            if (segs == cronometros[cronometro].tempoAtual) {
                 beep( true );
                 proximo();
-            }
-            else {
+            } else {
                 seg++;
-                if ( seg >= 57 && seg <= 59 && min == (cronometros[cronometro].tempoAtual - 1) )
+                segs++;
+                if ( seg >= (cronometros[cronometro].tempoSeg - 3) && seg <= cronometros[cronometro].tempoSeg && min >= (cronometros[cronometro].tempoMin - 1) )
                     beep();
-                if ( seg === 60 && min !== cronometros[cronometro].tempoAtual ) {
+                if ( seg === 60 && min !== cronometros[cronometro].tempoMin ) {
                     seg = 0;
                     min++;
                 }
-                if (seg == 0 && min == cronometros[cronometro].tempoAtual)
-                    beep( true );
                 changeTime(min, seg);
             }
         } else {
@@ -110,6 +157,7 @@ function timer() {
             }
             else {
                 seg--;
+                segs--;
                 if ( seg <= 3 && seg > 0 && min == 0 )
                     beep();
                 if ( seg <= 0 && min == 0 )
@@ -144,11 +192,63 @@ function init() {
     cronometro = 0;
 
     let CountInicial = new Cronometro(0, 4, 1, false);
-    cronometros.push(CountInicial);
+    cronometros.push( CountInicial );
 
-    for ( let i = 0; i < parseInt(inputRodadas.value); i++ ) {    
-        let CronometroInput = new Cronometro(inputTempo.value, 0, inputTempo.value, botaoTipo.checked);
-        cronometros.push(CronometroInput);
+    let valorInput,
+        minutosInput,
+        segundosInput,
+        CronometroInput;
+
+    if ( tipoCronometro == "cronometro" ) {
+
+        for ( let i = 0; i < parseInt( inputRodadasCro.value ); i++ ) {
+            valorInput = inputTempoCro.value;
+            minutosInput = Math.floor( valorInput / 60 );
+            segundosInput =  valorInput - (minutosInput * 60);
+            CronometroInput = new Cronometro(minutosInput, segundosInput, valorInput, botaoTipo.checked);
+            cronometros.push( CronometroInput );
+        }
+
+    } else {
+
+        if ( inputAquecimento.checked ) {
+            
+            valorInput = inputAquecimentoSerie.value;
+            minutosInput = Math.floor( valorInput / 60 );
+            segundosInput =  valorInput - (minutosInput * 60);
+            CronometroInput = new Cronometro(minutosInput, segundosInput, valorInput, botaoTipo.checked, "aquecimento");
+
+            cronometros.push( CronometroInput );
+
+        }
+
+        for ( let i = 0; i < parseInt( inputRodadasCro.value ); i++ ) {
+
+            valorInput = inputExercicioSerie.value;
+            minutosInput = Math.floor( valorInput / 60 );
+            segundosInput =  valorInput - (minutosInput * 60);
+            CronometroInput = new Cronometro(minutosInput, segundosInput, valorInput, botaoTipo.checked, "exercicio");
+
+            cronometros.push( CronometroInput );
+            
+            valorInput = inputDescansoSerie.value;
+            minutosInput = Math.floor( valorInput / 60 );
+            segundosInput =  valorInput - (minutosInput * 60);
+            CronometroInput = new Cronometro(minutosInput, segundosInput, valorInput, botaoTipo.checked, "descanso");
+
+            cronometros.push( CronometroInput );
+        }
+
+        if ( inputCooldown.checked ) {
+
+            valorInput = inputCooldownSerie.value;
+            minutosInput = Math.floor( valorInput / 60 );
+            segundosInput =  valorInput - (minutosInput * 60);
+            CronometroInput = new Cronometro(minutosInput, segundosInput, valorInput, botaoTipo.checked, "cooldown");
+
+            cronometros.push( CronometroInput );
+            
+        }
     }
     
     start();
@@ -184,3 +284,21 @@ function disable ( elem ) {
 function enable ( elem ) {
     elem.disabled = false;
 }
+
+let elem = document.querySelector('.sidenav');
+let instance = M.Sidenav.init( elem );
+
+let el = document.querySelector('.tabs');
+let instances = M.Tabs.init( el );
+
+let tabSerie = document.querySelector( "a[href='#serie']" );
+
+tabSerie.onclick = function() {
+    tipoCronometro = "serie";
+};
+
+let tabCronometro = document.querySelector( "a[href='#cronometro']" );
+
+tabCronometro.onclick = function() {
+    tipoCronometro = "cronometro";
+};
